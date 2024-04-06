e9a1e77457fd4d05b34d4d724294343c600796da
Jonas Coosemans <jonas.coosemans@colruytgroup.com>
2024-04-06 13:09:37 +0000

    first working version

--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,10 @@
 from ray.rllib.algorithms.ppo import PPOConfig
 from ray.tune.logger import pretty_print
 
+import gymnasium as gym
+
 import wandb
+from loguru import logger
 
 wandb.init(
     # set the wandb project where this run will be logged
@@ -17,11 +20,33 @@ algo = (
     .build()
 )
 
-for i in range(60):
+logger.info("starting training")
+
+for i in range(20):
+    logger.info(f"training step {i}")
     result = algo.train()
-    algo.validate_env()
-    print(pretty_print(result))
 
     if i % 5 == 0:
         checkpoint_dir = algo.save().checkpoint.path
-        print(f"Checkpoint saved in directory {checkpoint_dir}")
+        logger.debug(f"Checkpoint saved in directory {checkpoint_dir}")
+
+logger.success(f"succesfully trained network")
+
+env = gym.make("CartPole-v1", render_mode="rgb_array")
+
+obs, info = env.reset()
+
+logger.debug(f"started rendering")
+
+for i in range(500):
+    action = algo.compute_single_action(
+        observation=obs,
+    )
+    obs, reward, done, truncated, _ = env.step(action)
+    image = env.render()
+    
+    wandb.log({"example": wandb.Image(image)})
+
+    if done:
+        logger.success(f"done!")
+        break
