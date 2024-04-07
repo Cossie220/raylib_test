a1ba08a08fe45e7953fd7c45bde311424e3b6d8e
Jonas Coosemans <jonas.coosemans@colruytgroup.com>
2024-04-07 09:44:09 +0000

    fully working ray

--- scripts/test.py
+++ scripts/test.py
@@ -1,52 +1,83 @@
-from ray.rllib.algorithms.ppo import PPOConfig
+from ray.rllib.algorithms.ppo import PPOConfig, PPO
+from ray.rllib.algorithms.dqn import DQNConfig
+from ray.rllib.algorithms import Algorithm
 from ray.tune.logger import pretty_print
 
 import gymnasium as gym
 
+import numpy as np
+import cv2
+
 import wandb
 from loguru import logger
 
-wandb.init(
-    # set the wandb project where this run will be logged
-    project="ray_test",
-    sync_tensorboard=True
-)
+ENV = "MountainCar-v0"
+SIZE = (600, 400)
 
-algo = (
-    PPOConfig()
-    .rollouts(num_rollout_workers=8)
-    .resources(num_gpus=0)
-    .environment(env="CartPole-v1")
-    .build()
-)
 
-logger.info("starting training")
+def render_model(algorithm: Algorithm):
+    result = cv2.VideoWriter('test.webm',  
+                            cv2.VideoWriter_fourcc(*'VP90'), 
+                            50, SIZE) 
 
-for i in range(20):
-    logger.info(f"training step {i}")
-    result = algo.train()
+    env = gym.make(ENV, render_mode="rgb_array")
 
-    if i % 5 == 0:
-        checkpoint_dir = algo.save().checkpoint.path
-        logger.debug(f"Checkpoint saved in directory {checkpoint_dir}")
+    obs, info = env.reset()
 
-logger.success(f"succesfully trained network")
+    logger.debug(f"started rendering")
 
-env = gym.make("CartPole-v1", render_mode="rgb_array")
+    frames = []
+    image = None
 
-obs, info = env.reset()
+    for i in range(1000):
+        action = algorithm.compute_single_action(
+            observation=obs,
+            explore=False
+        )
+        obs, reward, done, truncated, _ = env.step(action)
+        image = env.render()
+        
+        image_array = np.asanyarray(image, dtype=np.uint8).reshape(400 ,600 ,3)
+        result.write(image_array)
+        frames.append(image_array)
 
-logger.debug(f"started rendering")
+        if done:
+            logger.success(f"done!")
+            break
 
-for i in range(500):
-    action = algo.compute_single_action(
-        observation=obs,
+    result.release()
+
+    wandb.log({"video": wandb.Video("test.webm", format="mp4")})
+
+
+def main():
+    wandb.init(
+        # set the wandb project where this run will be logged
+        project="ray_test",
+        sync_tensorboard=True
     )
-    obs, reward, done, truncated, _ = env.step(action)
-    image = env.render()
-    
-    wandb.log({"example": wandb.Image(image)})
 
-    if done:
-        logger.success(f"done!")
-        break
+    algo = (
+        DQNConfig()
+        .rollouts(num_rollout_workers=8)
+        .resources(num_gpus=0)
+        .environment(env=ENV)
+        .build()
+    )
+
+    logger.info("starting training")
+
+    for i in range(120):
+        logger.info(f"training step {i}")
+        result = algo.train()
+        if i % 5 == 0:
+            checkpoint_dir = algo.save().checkpoint.path
+            logger.debug(f"Checkpoint saved in directory {checkpoint_dir}")
+            render_model(algo)
+
+    logger.success(f"succesfully trained network")
+    render_model(algo)
+    wandb.finish()
+
+if __name__ == "__main__":
+    main()
